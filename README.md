1feb5b250e188ff4fa4f0baaeae267d501812fd5
tyoshida <tyoshida@solxyz.co.jp>
2017-11-19 11:45:37 +0900

    fix

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # VerseExtensionExampleHookBeforeSend
+![Version: 0.0.2](https://img.shields.io/badge/Version-0.0.2-green.svg)
+
+## Overview
+
 IBM Verse Extension example: hook before send 
+
+
+## Description
+
+This application displays a confirmation screen of e-mail address and attachments before sending e-mail from Verse.
+
+If the destination e-mail address is different from the domain of your e-mail address, you need to activate it by clicking on the e-mail address.
+Also, you must click on the attachment as well to enable it.
+Clicking on all destinations and attachments will enable send button.
+
+
+## Image
+![Image](https://github.com/chemp7/VerseExtensionExampleHookBeforeSend/blob/master/image/HookBeforeSend_Image01.png)
+
+![Image](https://github.com/chemp7/VerseExtensionExampleHookBeforeSend/blob/master/image/HookBeforeSend_Image02.png)
+
+## Install
+
+To add an application to Verse on-Cloud, you need to register it using the IBM App Registry. 
+You can modify the application.json file and register it.
+
+Please confirm on the official website how to register the application to Verse.
+https://ibmverse.github.io/verse-developer/developers/#registering-an-application-in-ibm-verse
+
+## Licence
+
+MIT
+
+
+## Author
+
+[Takeshi Yoshida](https://github.com/chemp7)
+
+
+## Releace
+
+2017/11/19 v0.0.2 fix
+
+2017/11/05 v0.0.1 initial
--- script.js
+++ script.js
@@ -5,11 +5,6 @@
     if (!isValidOrigin(event.origin)) {
       return;
     }
-    /*
-    if (event.origin !== "https://mail.notes.ap.collabserv.com"){
-      return;
-    }
-    */
     if(event.data) {
       if (event.data.verseApiType === 'com.ibm.verse.ping.application.loaded') {
         var loaded_message = {
@@ -18,158 +13,58 @@
         event.source.postMessage(loaded_message, event.origin);
       } else if (event.data.verseApiType === 'com.ibm.verse.action.clicked') {
         if(event.data.verseApiData.context) {
-          var sender = event.data.verseApiData.context.sender;
           var externalCount = 0;
-          // to
-          var toNode = document.getElementById("to");
-          var to = event.data.verseApiData.context.recipientTo;
-          if (to.length > 0) {
-            var i = 0;
-            for (var value of to) {
-              if (i > 0) {
-//                var pNode = document.createElement("p");
-//                toNode.appendChild(pNode);
-              }
-              
-              // internal/external
-              var senderEmailAddress = sender[0].emailAddress;
-              var domain = senderEmailAddress.substr(senderEmailAddress.lastIndexOf("@") + 1);
-              var externalFlag = "";
-              var nameInput = document.createElement("input");
-              var nameLabel = document.createElement("label");
-              var nameSpan = document.createElement("span");
-              if (value.emailAddress.slice(-(domain.length)) === domain) {
-                externalFlag = false;
-              } else {
-                externalFlag = true;
-                externalCount++;
-                nameInput.setAttribute("type", "checkbox");
-                nameInput.setAttribute("id", "cbToExternal_" + externalCount);
-                nameInput.className = "checkbox-input";
-                nameInput.name = "groupExternal";
-                nameLabel.setAttribute("for", "cbToExternal_" + externalCount);
-                nameSpan.className = "checkbox-span";
-              }
 
-              var emailAddress = value.emailAddress;
-              if (emailAddress === "") {
-                emailAddress = "No data";
-                nameSpan.classList.add("errorSpan");
-              }
-              nameSpan.textContent = value.displayName + " <" + emailAddress + ">";
-
-              if (externalFlag === false) {
-                  nameLabel.className = "internalLabel";
-                  nameLabel.appendChild(nameSpan);
-              } else {
-                  nameLabel.className = "externalLabel";
-                  nameLabel.appendChild(nameInput);
-                  nameLabel.appendChild(nameSpan);
-              }
-              var ctrlPostMessage = function(nm, event) {
-                return function(evt) {
-                  var response_message = {};
-                  if (checkOn(nm)) {
-                    response_message = {
-                      verseApiType: 'com.ibm.verse.message.enable.send'
-                    };
-                  } else {
-                    response_message = {
-                      verseApiType: 'com.ibm.verse.message.disable.send'
-                    };
-                  }
-                  event.source.postMessage(response_message, event.origin);
-                }
-              }
-              nameLabel.addEventListener("change", ctrlPostMessage(nameInput.name, event), false);
-              toNode.appendChild(nameLabel);
-              i++;
+          // To
+          if (event.data.verseApiData.context.hasOwnProperty("recipientTo")) {
+            if (event.data.verseApiData.context.recipientTo.length > 0) {
+              externalCount = addNodeEmailAddress("To", event);
             }
           }
 
-          // cc
-          var ccNode = document.getElementById("cc");
-          var cc = event.data.verseApiData.context.recipientCC;
-          if (cc.length > 0) {
-            var j = 0;
-            for (var value of cc) {
-              if (j > 0) {
-//                var pNode = document.createElement("p");
-//                ccNode.appendChild(pNode);
-              }
-              
-              // internal/external
-              var senderEmailAddress = sender[0].emailAddress;
-              var domain = senderEmailAddress.substr(senderEmailAddress.lastIndexOf("@") + 1);
-              var externalFlag = "";
-              var nameInput = document.createElement("input");
-              var nameLabel = document.createElement("label");
-              var nameSpan = document.createElement("span");
-              if (value.emailAddress.slice(-(domain.length)) === domain) {
-                externalFlag = false;
-              } else {
-                externalFlag = true;
-                externalCount++;
-                nameInput.setAttribute("type", "checkbox");
-                nameInput.setAttribute("id", "cbCcExternal_" + externalCount);
-                nameInput.className = "checkbox-input";
-                nameInput.name = "groupExternal";
-                nameLabel.setAttribute("for", "cbCcExternal_" + externalCount);
-                nameSpan.className = "checkbox-span";
-              }
+          // CC
+          if (event.data.verseApiData.context.hasOwnProperty("recipientCC")) {
+            if (event.data.verseApiData.context.recipientCC.length > 0) {
+              externalCount = addNodeEmailAddress("CC", event);
+            }
+          }
 
-              var emailAddress = value.emailAddress;
-              if (emailAddress === "") {
-                emailAddress = "No data";
-                nameSpan.classList.add("errorSpan");
-              }
-              nameSpan.textContent = value.displayName + " <" + emailAddress + ">";
-
-              if (externalFlag === false) {
-                  nameLabel.className = "internalLabel";
-                  nameLabel.appendChild(nameSpan);
-              } else {
-                  nameLabel.className = "externalLabel";
-                  nameLabel.appendChild(nameInput);
-                  nameLabel.appendChild(nameSpan);
-              }
-              var ctrlPostMessage = function(nm, event) {
-                return function(evt) {
-                  var response_message = {};
-                  if (checkOn(nm)) {
-                    response_message = {
-                      verseApiType: 'com.ibm.verse.message.enable.send'
-                    };
-                  } else {
-                    response_message = {
-                      verseApiType: 'com.ibm.verse.message.disable.send'
-                    };
-                  }
-                  event.source.postMessage(response_message, event.origin);
-                }
-              }
-              nameLabel.addEventListener("change", ctrlPostMessage(nameInput.name, event), false);
-              ccNode.appendChild(nameLabel);
-              j++;
+          // Bcc
+          if (event.data.verseApiData.context.hasOwnProperty("recipientBcc")) {
+            if (event.data.verseApiData.context.recipientBcc.length > 0) {
+              externalCount = addNodeEmailAddress("Bcc", event);
             }
           }
 
           // subject
-      	  var subjectNode = document.getElementById("subject");
+      	  var subjectNode = document.getElementById("Subject");
   	      subjectNode.innerText = event.data.verseApiData.context.subject;
 
+          // Attachments
+          if (event.data.verseApiData.context.hasOwnProperty("attachments")) {
+            if (event.data.verseApiData.context.attachments.length > 0) {
+              externalCount = addNodeAttachments(event);
+            }
+          }
+
           // json
-//      	  var jsonNode = document.getElementById("json");
-//  	      jsonNode.innerText = jsonNode.innerText + "\n" + JSON.stringify(event.data, null, 2);
+      	  var jsonNode = document.getElementById("json");
+  	      jsonNode.innerText = jsonNode.innerText + "\n" + JSON.stringify(event.data, null, 2);
 
           if (externalCount > 0) {
             var headerNode = document.getElementById("header");
             var hrTopNode = document.getElementById("hr-top");
             var infoNode = document.createElement("span");
             infoNode.className = "information";
-            var infoNodeText = document.createTextNode("社外の送信先は確認後、チェックマークをを付けてください。");
+            var infoNodeText = document.createTextNode("[社外の送信先], [添付ファイル] を確認（クリック）してください。");
             infoNode.appendChild(infoNodeText);
+            infoNode.id = "info";
             headerNode.insertBefore(infoNode, hrTopNode);
+          } else {
+            var response_message = {
+              verseApiType: 'com.ibm.verse.message.enable.send'
+            };
+            event.source.postMessage(response_message, event.origin);
           }
 
         } else {
@@ -181,9 +76,158 @@
     }
   }, false);
 
-  function checkOn( groupName ) {
+  function addNodeEmailAddress(recipientType, event) {
+    var sender = event.data.verseApiData.context.sender;
+    var recipientNode = document.getElementById(recipientType);
+    var recipientData = event.data.verseApiData.context["recipient" + recipientType];
+    var externalCount = 0;
+    for (var value of recipientData) {
+      // internal/external
+      var senderEmailAddress = sender[0].emailAddress;
+      var domain = senderEmailAddress.substr(senderEmailAddress.lastIndexOf("@") + 1);
+      var externalFlag = "";
+      var nameInput = document.createElement("input");
+      var nameLabel = document.createElement("label");
+      var nameSpan = document.createElement("span");
+      if (value.emailAddress.slice(-(domain.length)) === domain) {
+        externalFlag = false;
+      } else {
+        externalFlag = true;
+        externalCount++;
+        nameInput.setAttribute("type", "checkbox");
+        nameInput.setAttribute("id", recipientType + "_cbExternal_" + externalCount);
+        nameInput.className = "checkbox-input";
+        nameInput.name = "groupExternal";
+        nameLabel.setAttribute("for", recipientType + "_cbExternal_" + externalCount);
+        nameLabel.id = recipientType + "_nlExternal_" + externalCount;
+        nameSpan.className = "checkbox-span";
+      }
+      
+      var emailAddress = value.emailAddress;
+      if (emailAddress === "") {
+        emailAddress = "No data";
+        nameSpan.classList.add("errorSpan");
+      }
+      if (value.displayName === emailAddress) {
+        nameSpan.textContent = emailAddress;
+      } else {
+        nameSpan.textContent = value.displayName + " <" + emailAddress + ">";
+      }
+        
+      if (externalFlag === false) {
+        nameLabel.className = "internalLabel";
+        nameLabel.appendChild(nameSpan);
+      } else {
+        nameLabel.className = "externalLabel";
+        nameLabel.appendChild(nameInput);
+        nameLabel.appendChild(nameSpan);
+      }
+      var ctrlPostMessage = function(labelId, nm, id, event) {
+        return function(evt) {
+          var label = document.getElementById(labelId);
+          label.className = "externalLabel_confirmed";
+
+          var checkbox = document.getElementById(id);
+          if (checkbox.checked) {
+            checkbox.disabled = true;
+          }
+        
+          var response_message = {};
+          if (checkOn(nm)) {
+            response_message = {
+              verseApiType: 'com.ibm.verse.message.enable.send'
+            };
+            var infoNode = document.getElementById("info");
+            infoNode.textContent = "確認が完了しました。";
+          } else {
+            /*
+            response_message = {
+              verseApiType: 'com.ibm.verse.message.disable.send'
+            };
+            */
+          }
+          event.source.postMessage(response_message, event.origin);
+        }
+      }
+      nameLabel.addEventListener("change", ctrlPostMessage(nameLabel.id, nameInput.name, nameInput.id, event), false);
+      recipientNode.appendChild(nameLabel);
+    }
+
+    return externalCount;
+  }
+
+  function addNodeAttachments(event) {
+    var recipientNode = document.getElementById("Attachments");
+    var recipientData = event.data.verseApiData.context.attachments;
+    var externalCount = 0;
+    for (var value of recipientData) {
+      // internal/external
+      var externalFlag = "";
+      var nameInput = document.createElement("input");
+      var nameLabel = document.createElement("label");
+      var nameSpan = document.createElement("span");
+
+      externalFlag = true;
+      externalCount++;
+      nameInput.setAttribute("type", "checkbox");
+      nameInput.setAttribute("id", "attchments_cbExternal_" + externalCount);
+      nameInput.className = "checkbox-input";
+      nameInput.name = "groupExternal";
+      nameLabel.setAttribute("for", "attchments_cbExternal_" + externalCount);
+      nameLabel.id = "attchments_nlExternal_" + externalCount;
+      nameSpan.className = "checkbox-span";
+
+      var sizeString = 0;
+      if (value.size >= 1048576) {
+        sizeString = Math.ceil(value.size / 1048576) + " MB"
+      } else if (value.size >= 1024 && value.size < 1048576) {
+        sizeString = Math.ceil(value.size / 1024) + " KB"
+      } else {
+        sizeString = value.size + " byte"
+      }
+      nameSpan.textContent = value.name + " [" + sizeString + "]";
+        
+      nameLabel.className = "externalLabel";
+      nameLabel.appendChild(nameInput);
+      nameLabel.appendChild(nameSpan);
+
+      var ctrlPostMessage = function(labelId, nm, id, event) {
+        return function(evt) {
+          var label = document.getElementById(labelId);
+          label.className = "externalLabel_confirmed";
+
+          var checkbox = document.getElementById(id);
+          if (checkbox.checked) {
+            checkbox.disabled = true;
+          }
+        
+          var response_message = {};
+          if (checkOn(nm)) {
+            response_message = {
+              verseApiType: 'com.ibm.verse.message.enable.send'
+            };
+            var infoNode = document.getElementById("info");
+            infoNode.textContent = "確認が完了しました。";
+          } else {
+            /*
+            response_message = {
+              verseApiType: 'com.ibm.verse.message.disable.send'
+            };
+            */
+          }
+          event.source.postMessage(response_message, event.origin);
+        }
+      }
+      nameLabel.addEventListener("change", ctrlPostMessage(nameLabel.id, nameInput.name, nameInput.id, event), false);
+      recipientNode.appendChild(nameLabel);
+    }
+
+    return externalCount;
+  }
+
+  function checkOn(groupName) {
     var result = false;
-    var groupCheckboxes = document.getElementsByName( groupName );
+    var groupCheckboxes = document.getElementsByName(groupName);
     var checkedCount = 0;
     var uncheckedCount = 0;
     for (var i = 0; i < groupCheckboxes.length; i++ ){
